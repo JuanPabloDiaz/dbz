7001c7553fe315f2ffe92af4cb3b86fb3083343b
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-08 00:13:04 -0400

    Refactor import paths to use absolute imports and clean up code formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,4 @@ export default defineConfig({
   integrations: [tailwind(), icon()],
   output: 'static',
   site: 'https://dball.netlify.app',
-  baseUrl: '.',
-  paths: {
-    '@src/*': ['src/*'],
-    '@components/*': ['src/components/*'],
-    '@layouts/*': ['src/layouts/*'],
-    '@pages/*': ['src/pages/*'],
-    '@tests/*': ['src/tests/*'],
-    '@types/*': ['src/types/*'],
-    '@utils/*': ['src/utils/*'],
-  },
 });
