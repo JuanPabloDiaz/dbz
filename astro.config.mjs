e0b4557f5b96cd7ecb81a0bd53f72ad96c4c753f
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-07-08 00:41:48 -0400

    Update Astro configuration and dependencies; refactor import paths for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: 'static',
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://dball.netlify.app',
 });
